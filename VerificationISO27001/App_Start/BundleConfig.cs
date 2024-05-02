4d1b142b887de0dd02f44912cd8d0517bfce9b11
Dmitriy Voicu <voiku173dmitry@gmail.com>
2024-05-02 12:24:28 +0300

    anchor was added

--- VerificationISO27001/App_Start/BundleConfig.cs
+++ VerificationISO27001/App_Start/BundleConfig.cs
@@ -15,6 +15,10 @@ namespace VerificationISO27001.App_Start
                 "~/Content/myStyles/list-groups.css", new CssRewriteUrlTransform()));
             bundles.Add(new StyleBundle("~/bundles/main/css").Include(
                 "~/Content/myStyles/style.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/fonta/css").Include(
+                "~/Content/font-awesome.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/fontam/css").Include(
+                "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
 
             //scripts
             bundles.Add(new ScriptBundle("~/bundles/script/js").Include(
